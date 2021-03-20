fb86dbee8f89e9e3fa1fd6d6d684325a11099173
Mac <65713232+machooper@users.noreply.github.com>
2021-03-20 15:12:44 +0000

    Update copy.js

--- comps/layout/copy.js
+++ comps/layout/copy.js
@@ -3,5 +3,5 @@ import config from '../../config'
 export default function Copy() {
 	const yr = new Date().getFullYear();
 	return (
-		<Link href="/"><p style={{fontWeight: '300'}}>Produced by &copy; <a>Adapt Studio</a> {yr}</p></Link>
+		<Link href="/"><p style={{fontWeight: '300'}}>Produced by &copy; <span>Adapt Studio</span> {yr}</p></Link>
 )}
