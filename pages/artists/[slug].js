0ca9e10f3a2019d8450196b65fe15d1486edb506
Mac Develops <65713232+machooper@users.noreply.github.com>
2021-03-20 14:57:43 +0000

    fix: button positioning on desktop devices (artists page)

--- pages/artists/[slug].js
+++ pages/artists/[slug].js
@@ -54,10 +54,15 @@ export default function Post(post) {
                 .container img {
                     width: 100%;
                 }
-                @media(min-width: 760px) {
+                @media(min-width: 600px) {
                     .container img {
                         width: 400px;
                     }    
+                    .text {
+                        display: flex;
+                        flex-direction: column;
+                        align-items: center;
+                    }
                     .releases {
                         margin-top: 2rem;
                     }
